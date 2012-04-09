d172c62ec7456a11794496b46118e0f42d82d866
happycamper <happycamper.sw@gmail.com>
2012-04-09 16:57:22 -0700

    bugfix: debugger snuck in

--- Ruby/lib/profiler/rails_patches.rb
+++
@@ -1,3 +0,0 @@
-if defined?(::Rails) && ::Rails::VERSION::MAJOR.to_i == 3
-  ::Rack::MiniProfiler.profile_method(ActionController::Base, :process) {|action| "executing action #{action}"}
-end
--- Ruby/lib/rack-mini-profiler.rb
+++ Ruby/lib/rack-mini-profiler.rb
@@ -1,3 +1,2 @@
 require File.expand_path('profiler/profiler', File.dirname(__FILE__) )
 require File.expand_path('profiler/sql_patches', File.dirname(__FILE__) )
-require File.expand_path('profiler/rails_patches', File.dirname(__FILE__) )
