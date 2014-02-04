7824a5a7ef3efc38be47e866bf159323fd24b634
happycamper <happycamper.sw@gmail.com>
2014-02-04 15:27:35 +0200

    Code Cleanup

--- StackExchange.Profiling.EntityFramework/ObjectContextUtils.cs
+++ StackExchange.Profiling.EntityFramework/ObjectContextUtils.cs
@@ -1,7 +1,8 @@
-﻿namespace StackExchange.Profiling.Data
+﻿using System.Collections.Concurrent;
+
+namespace StackExchange.Profiling.Data
 {
     using System;
-    using System.Collections.Generic;
     using System.Data.Common;
     using System.Data.EntityClient;
     using System.Data.Metadata.Edm;
@@ -33,7 +34,7 @@
             /// <summary>
             /// The hash code.
             /// </summary>
-            private int _hashCode;
+            private readonly int _hashCode;
 
             /// <summary>
             /// Initialises a new instance of the <see cref="MetadataCacheKey"/> class. 
@@ -54,7 +55,7 @@
 
                 _assemblies = assemblies;
                 _paths = paths;
-                CreateHashCode();
+                _hashCode = CreateHashCode();
             }
 
             /// <summary>
@@ -76,7 +77,7 @@
                 _paths[0] = string.Format(Pattern, assemblyName, edmxName, "ssdl");
                 _paths[1] = string.Format(Pattern, assemblyName, edmxName, "msl");
                 _paths[2] = string.Format(Pattern, assemblyName, edmxName, "csdl");
-                CreateHashCode();
+                _hashCode = CreateHashCode();
             }
 
             /// <summary>
@@ -124,14 +125,14 @@
                     return false;
                 if (_assemblies.Count() != cacheKey._assemblies.Count() || _paths.Count() != cacheKey._paths.Count())
                     return false;
-                int i = 0;
-                for (i = 0; i < _assemblies.Count(); i++)
+
+                for (int i = 0; i < _assemblies.Count(); i++)
                 {
                     if (_assemblies[i] != cacheKey._assemblies[i])
                         return false;
                 }
 
-                for (i = 0; i < _paths.Count(); i++)
+                for (int i = 0; i < _paths.Count(); i++)
                 {
                     if (_paths[i] != cacheKey._paths[i])
                         return false;
@@ -143,13 +144,14 @@
             /// <summary>
             /// create the hash code.
             /// </summary>
-            private void CreateHashCode()
+            private int CreateHashCode()
             {
-                _hashCode = 19;
+                var hashCode = 19;
                 foreach (var assembly in _assemblies)
-                    _hashCode = (3 * _hashCode) ^ assembly.GetHashCode();
+                    hashCode = (3 * hashCode) ^ assembly.GetHashCode();
                 foreach (var path in _paths)
-                    _hashCode = (3 * _hashCode) ^ path.GetHashCode();
+                    hashCode = (3 * hashCode) ^ path.GetHashCode();
+                return hashCode;
             }
         }
 
@@ -161,7 +163,7 @@
         /// <returns>the object context.</returns>
         public static T CreateObjectContext<T>(this DbConnection connection) where T : System.Data.Objects.ObjectContext
         {
-            return CreateObjectContext<T>(connection, new MetadataCacheKey(new Assembly[] { typeof(T).Assembly }, new string[] { "res://*/" }));
+            return CreateObjectContext<T>(connection, new MetadataCacheKey(new [] { typeof(T).Assembly }, new [] { "res://*/" }));
         }
 
         /// <summary>
@@ -185,7 +187,7 @@
         /// <returns>the object context</returns>
         public static T CreateObjectContext<T>(this DbConnection connection, string[] paths) where T : System.Data.Objects.ObjectContext
         {
-            return CreateObjectContext<T>(connection, new MetadataCacheKey(new Assembly[] { typeof(T).Assembly }, paths));
+            return CreateObjectContext<T>(connection, new MetadataCacheKey(new [] { typeof(T).Assembly }, paths));
         }
 
         /// <summary>
@@ -201,9 +203,11 @@
             var factory = DbProviderServices.GetProviderFactory(connection);
 
             var itemCollection = workspace.GetItemCollection(DataSpace.SSpace);
-            itemCollection.GetType().GetField(
+            var fieldInfo = itemCollection.GetType().GetField(
                 "_providerFactory", // <==== big fat ugly hack
-                BindingFlags.NonPublic | BindingFlags.Instance).SetValue(itemCollection, factory);
+                BindingFlags.NonPublic | BindingFlags.Instance);
+            if (fieldInfo != null)
+                fieldInfo.SetValue(itemCollection, factory);
 
             var ec = new EntityConnection(workspace, connection);
             return CtorCache<T, EntityConnection>.Ctor(ec);
@@ -217,7 +221,7 @@
         public static T GetProfiledContext<T>() where T : System.Data.Objects.ObjectContext
         {
             var conn = new EFProfiledDbConnection(GetStoreConnection<T>(), MiniProfiler.Current);
-            return ObjectContextUtils.CreateObjectContext<T>(conn);
+            return CreateObjectContext<T>(conn);
         }
 
         /// <summary>
@@ -321,14 +325,14 @@
             /// <summary>
             /// The _workspaces.
             /// </summary>
-            private static readonly Dictionary<MetadataCacheKey, MetadataWorkspace> Workspaces;
+            private static readonly ConcurrentDictionary<MetadataCacheKey, MetadataWorkspace> Workspaces;
 
             /// <summary>
             /// Initialises static members of the <see cref="MetadataCache"/> class.
             /// </summary>
             static MetadataCache()
             {
-                Workspaces = new Dictionary<MetadataCacheKey, MetadataWorkspace>();
+                Workspaces = new ConcurrentDictionary<MetadataCacheKey, MetadataWorkspace>();
             }
 
             /// <summary>
@@ -338,10 +342,12 @@
             /// <returns>the meta data workspace.</returns>
             public static MetadataWorkspace GetWorkspace(MetadataCacheKey key)
             {
-                if (Workspaces.ContainsKey(key))
-                    return Workspaces[key];
-                Workspaces[key] = new MetadataWorkspace(key.Paths, key.Assemblies);
-                return Workspaces[key];
+                return Workspaces.GetOrAdd(key, CreateWorkspace);
+            }
+
+            private static MetadataWorkspace CreateWorkspace(MetadataCacheKey key)
+            {
+                return new MetadataWorkspace(key.Paths, key.Assemblies);
             }
         }
     }
